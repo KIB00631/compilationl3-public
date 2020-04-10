70c607e31fdfa41d34ed28c1ca05e3f8418a1389
Vincent malkowiak <vincent.malkowiak@iutnice.unice.fr>
2020-04-10 22:52:03 +0200

    V1 du ColorGraphe et maj README

--- src/util/graph/ColorGraph.java
+++ src/util/graph/ColorGraph.java
@@ -3,14 +3,14 @@ package util.graph;
 import util.intset.IntSet;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.List;
+
 
 public class ColorGraph {
     public  Graph          G;
     public  int            R;
     public  int            K;
-    private Queue<Integer> pile; // c'est une file pas une pile :D
+    private Stack<Integer> pile; // c'est une file pas une pile :D
     public  IntSet         enleves;
     public  IntSet         deborde;
     public  int[]          couleur;
@@ -20,7 +20,7 @@ public class ColorGraph {
     public ColorGraph(Graph G, int K, int[] phi){
         this.G       = G;
         this.K       = K;
-        pile         = Collections.asLifoQueue(new ArrayDeque<>(G.nodeCount()));
+        pile         = new Stack<>();
         R            = G.nodeCount();
         couleur      = new int[R];
         enleves      = new IntSet(R);
@@ -41,11 +41,12 @@ public class ColorGraph {
     /* associe une couleur à tous les sommets se trouvant dans la pile */
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public void selection()
-    {
+    public void selection() {
         while (!pile.isEmpty()) {
-            int i = pile.poll();
-            couleur[i] = choisisCouleur(couleursVoisins(i));
+            int i = pile.pop();
+            IntSet c = this.couleursVoisins(i);
+            if(c.getSize() != this.K)
+                couleur[i] = choisisCouleur(couleursVoisins(i));
         }
     }
 
@@ -53,8 +54,7 @@ public class ColorGraph {
     /* récupère les couleurs des voisins de t */
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public IntSet couleursVoisins(int t)
-    {
+    public IntSet couleursVoisins(int t) {
         Node node = G.nodeArray()[t];
         IntSet color = new IntSet(K);
         var nodes = List.of(int2Node);
@@ -71,8 +71,7 @@ public class ColorGraph {
     /* recherche une couleur absente de colorSet */
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public int choisisCouleur(IntSet colorSet)
-    {
+    public int choisisCouleur(IntSet colorSet){
         for (int i = 0; i < colorSet.getSize(); i++)
             if(!colorSet.isMember(i))
                 return i;
@@ -83,16 +82,20 @@ public class ColorGraph {
     /* calcule le nombre de voisins du sommet t */
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public int nbVoisins(int t)
-    {
-        int nbNeighbours = 0;
-        Set<Node> exists = pile.stream().map(c -> int2Node[c]).collect(Collectors.toCollection(HashSet::new));
-        for(var succ = G.nodeArray()[t].succ() ; succ != null ; succ = succ.tail)
-            if(!exists.contains(succ.head))
-                ++nbNeighbours;
-        return nbNeighbours;
+    public int nbVoisins(int t) {
+        Node node = int2Node[t];
+        return node.len(node.adj());
     }
 
+
+    private int nbPreCo() {
+        int nb = 0;
+        for (int c : couleur)
+            if (c != NOCOLOR)
+                nb++;
+
+        return nb;
+    }
     /*-------------------------------------------------------------------------------------------------------------*/
     /* simplifie le graphe d'interférence g                                                                        */
     /* la simplification consiste à enlever du graphe les temporaires qui ont moins de k voisins                   */
@@ -100,40 +103,36 @@ public class ColorGraph {
     /* à la fin du processus, le graphe peut ne pas être vide, il s'agit des temporaires qui ont au moins k voisin */
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public int simplification()
-    {
-        int step = 0;
-        boolean isUpdated = true;
-        while (isUpdated) {
-            isUpdated = false;
-            Iterator<Integer> pendingNode = IntStream.range(0, int2Node.length).filter(k -> !enleves.isMember(k)).iterator();
-            while (pendingNode.hasNext()) {
-                int i = pendingNode.next();
-                if (nbVoisins(i) < K) {
-                    ++step;
-                    isUpdated = true;
-                    pile.add(i);
-                    enleves.add(i);
+    public void simplification() {
+        boolean asChanged = true;
+        while (pile.size() != (this.R - this.nbPreCo()) && asChanged) {
+            asChanged = false;
+            for (int i=0 ; i < R ; i++) {
+                if (!enleves.isMember(i)) {
+                    if (nbVoisins(i) < K && couleur[i] == NOCOLOR) {
+                        pile.push(i);
+                        enleves.add(i);
+                        asChanged = true;
+                    }
                 }
             }
         }
-        return step;
     }
 
     /*-------------------------------------------------------------------------------------------------------------*/
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public void debordement()
-    {
-        Iterator<Integer> pendingNode = IntStream.range(0, int2Node.length).filter(k -> !enleves.isMember(k)).iterator();
-        while (pendingNode.hasNext()) {
-            int i = pendingNode.next();
-            if(enleves.isMember(i) || nbVoisins(i) < K)
-                continue;
-            pile.add(i);
-            enleves.add(i);
-            deborde.add(i);
-            simplification();
+    public void debordement() {
+        while(pile.size() != 0){
+            for(Node node : int2Node){
+                if(enleves.isMember(node.mykey)){
+                    pile.push(node.mykey);
+                    enleves.add(node.mykey);
+                    deborde.add(node.mykey);
+                    simplification();
+                    break;
+                }
+            }
         }
     }
 
@@ -141,15 +140,13 @@ public class ColorGraph {
     /*-------------------------------------------------------------------------------------------------------------*/
     /*-------------------------------------------------------------------------------------------------------------*/
 
-    public void coloration()
-    {
+    public void coloration() {
         this.simplification();
         this.debordement();
         this.selection();
     }
 
-    void affiche()
-    {
+    void affiche() {
         System.out.println("vertex\tcolor");
         for(int i = 0; i < R; i++){
             System.out.println(i + "\t" + couleur[i]);
